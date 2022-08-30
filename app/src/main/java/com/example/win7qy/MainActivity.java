2e4440b957f31b5302d3adb2b1928df9b08fc451
chusiping <chusiping@163.com>
2022-08-30 18:08:08 +0800

    2022-8-30 18:07 手机端载入url地址展示的程序,最简单的

---
+++ app/src/main/java/com/example/win7qy/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.win7qy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        webView = (WebView) findViewById(R.id.wv1);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.loadUrl("http://14.23.86.186:8404/vhost/peixun/code.html");
+    }
+}
